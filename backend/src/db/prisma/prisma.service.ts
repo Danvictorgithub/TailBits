fb1645b8bda011ceba119ca811ddd536cd6428b9
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-12-15 23:25:54 +0800

    updated from bcrypt to argon2

--- backend/src/db/prisma/prisma.service.ts
+++ backend/src/db/prisma/prisma.service.ts
@@ -4,18 +4,6 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
-        // this.$extends({
-        //     result: {
-        //         profile: {
-        //             fullName: {
-        //                 needs: { firstName: true, lastName: true },
-        //                 compute(profile) {
-        //                     return `${profile.firstName} ${profile.lastName}`;
-        //                 },
-        //             },
-        //         },
-        //     },
-        // });
         await this.$connect();
         console.log("Prisma Initialized");
     }
